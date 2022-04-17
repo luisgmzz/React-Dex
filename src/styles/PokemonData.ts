77dd9cb57ec260c81c3f38fc5691cef95062c105
SoulMaster71 <69698862+SoulMaster71@users.noreply.github.com>
2022-04-17 13:52:47 +0200

    add styles

---
+++ src/styles/PokemonData.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const PokemonName = styled.h3`
+  color: ${({ color }) => color};
+`;
+export const PokemonSprite = styled.img`
+  background-color: #a1c9f0;
+  margin: 10px;
+  border: 2px solid #50515a;
+  border-radius: 5px;
+`;
