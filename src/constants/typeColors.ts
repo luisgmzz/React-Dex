3bd664406bfb939789349aec31842be1410a880e
SoulMaster71 <69698862+SoulMaster71@users.noreply.github.com>
2022-04-23 21:36:59 +0200

    update type colors

---
+++ src/constants/typeColors.ts
@@ -0,0 +1,21 @@
+export default {
+  normal: "#A0A978",
+  fire: "#DB8E1D",
+  water: "royalblue",
+  grass: "#77C857",
+  flying: "#A592D3",
+  fighting: "firebrick",
+  poison: "",
+  electric: "yellow",
+  ground: "burlywood",
+  rock: "#B6A123",
+  psychic: "#F05881",
+  ice: "#92D6DA",
+  bug: "yellowgreen",
+  ghost: "#69598E",
+  steel: "#AFB3CF",
+  dragon: "mediumslateBlue",
+  dark: "slategrey",
+  fairy: "#FF88EE",
+  bird: "#FFFFFF"
+};
