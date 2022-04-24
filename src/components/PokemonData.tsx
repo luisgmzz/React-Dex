ae9e3f74c747876801b853787fc7fd59009b82e8
SoulMaster71 <69698862+SoulMaster71@users.noreply.github.com>
2022-04-24 14:00:37 +0200

    update pokemon display styled

--- src/components/PokemonData.tsx
+++ src/components/PokemonData.tsx
@@ -1,4 +1,4 @@
-import { PokemonName, PokemonSprite } from "../styles/PokemonData";
+import { PokemonName, PokemonSprite, StyledDiv } from "../styles/PokemonData";
 import Capitalize from "../utils/capitalize";
 import typeColors from "../constants/typeColors";
 import PokemonTypes from "./PokemonTypes";
@@ -9,12 +9,12 @@ type Props = {
 
 export default function PokemonData({ pokemon }: Props) {
   return (
-    <div>
+    <StyledDiv>
       <PokemonName color={typeColors[pokemon.types[0]]}>
         {Capitalize(pokemon.name)}
       </PokemonName>
       <PokemonSprite src={pokemon.sprite} alt={`Sprite de ${pokemon.name}`} />
       <PokemonTypes types={pokemon.types} />
-    </div>
+    </StyledDiv>
   );
 }
--- src/styles/PokemonData.ts
+++ src/styles/PokemonData.ts
@@ -1,11 +1,32 @@
 import styled from "styled-components";
 
+export const StyledDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
 export const PokemonName = styled.h3`
-  color: ${({ color }) => color};
+  margin: 20px;
+  padding-bottom: 1rem;
+  padding-right: 2rem;
+  padding-left: 2rem;
+
+  font-size: 200%;
+  font-family: "Pokemon Solid", sans-serif;
+  letter-spacing: 3px;
+  color: white;
+  -webkit-text-stroke: 0.2px grey;
+
+  background-color: ${({ color }) => color};
+  border: 2.5px solid grey;
+  border-radius: 10px;
 `;
 export const PokemonSprite = styled.img`
+  margin-bottom: 50px;
+  width: 25%;
+
   background-color: #a1c9f0;
-  margin: 10px;
   border: 2px solid #50515a;
   border-radius: 5px;
 `;
