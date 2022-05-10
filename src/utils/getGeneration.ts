8e82970c70e3e6a6be27d86654e860ccdfb0d93a
SoulMaster71 <69698862+SoulMaster71@users.noreply.github.com>
2022-05-10 18:08:06 +0200

    add generation to type Pokemon

--- src/utils/getGeneration.ts
+++ src/utils/getGeneration.ts
@@ -2,5 +2,5 @@ export default function getGeneration(id: number): number {
   const generations = [151, 251, 386, 493, 649, 721, 809, 905];
 
   const gen = generations.find(gen => gen > id) || 100;
-  return generations.indexOf(gen);
+  return generations.indexOf(gen) + 1;
 }
