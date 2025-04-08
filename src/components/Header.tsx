87b73a3eee0ae03224e0a6d892e2f6cd85ba6608
luisjg04 <luisjg04@ucm.es>
2025-04-08 21:02:45 +0200

    Header actualizado

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,13 +3,22 @@ import { StyledHeader } from "../styles/Wrappers";
 import NavLink from "./NavLink";
 
 export default function Header() {
+  const routes = [
+    {
+      name: "Pokemon Finder",
+      route: "/find"
+    },
+    {
+      name: "Guess the Pokemon",
+      route: "/guess"
+    },
+  ]
+
   return (
     <StyledHeader>
       <nav>
         <ul>
-          <NavLink route="/find">Pokemon Finder</NavLink>
-          <NavLink route="/guess">Guess The Pokemon</NavLink>
-          <NavLink route="/pokerdle">Pokerdle</NavLink>
+          {routes.map(({name, route}) => <NavLink route={route}>{name}</NavLink>)}
         </ul>
       </nav>
     </StyledHeader>
